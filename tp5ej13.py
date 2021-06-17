fd5db4e8bd66a7bfbaad2a3622664d5b0f2ba7ef
Hanchut <Galoorellana2@hotmail.com>
2021-06-16 23:02:10 -0300

    Update 1.2 - Ejercicio 13
    Ejercicio 13 finalizado y subido para correcciones del profe.

--- tp5ej13.py
+++ tp5ej13.py
@@ -6,29 +6,41 @@
 
 # Reemplazar por las funciones del ejercicio
 
-def construccion_texto(mensaje):
-    
-    texto = []
-    for i in range(1):
-        palabra = str(input(mensaje + '-'))
-        texto.append(palabra)
-    return texto
+class SinUbicacion(Exception):
+    """Esta es la excepcion para las palabras que no pudieron ser ubicadas"""
+    pass
 
 def ubicacion_palabra(texto, palabra):
     
-    contador = 0
-    misma_palabra = 0
-    for i in texto:
-        for i in palabra:
-            return misma_palabra == misma_palabra + 1
+    ubicacion = 0
+    letra = 0
+    misma_letra = 0
+    for i in range(len(texto)):
+        if letra < len(palabra) and texto[i] == palabra[letra]:
+            if letra == 0:
+                ubicacion = i
+                letra = letra + 1
+                misma_letra = misma_letra + 1
+            else:
+                misma_letra = misma_letra + 1
+                letra = letra + 1
+        elif misma_letra == len(palabra) and texto[i] == ' ':
+            return ubicacion
+        else:
+            ubicacion = 0
+            letra = 0
+            misma_letra = 0
+    if misma_letra == len(palabra):
+        return ubicacion
+    raise SinUbicacion('La palabra que desea encontrar no existe o no pudo ser ubicada.')
     
 def prueba():
     
     print('Bienvenido a mi codigo!')
-    texto = construccion_texto('\nIngrese el texto que desea comparar: ')
+    texto = str(input('\nIngrese el texto que desea comparar: '))
     palabra = str(input('\nPalabra que desee saber su ubicacion y si esta dentro del texto: '))
     ubicacion = ubicacion_palabra(texto, palabra)
-    print(f'\nLa palabra esta ubicadad en la posicion nro: {ubicacion}.')
+    print(f'\nLa palabra esta ubicada en la posicion nro: {ubicacion}.')
 
 if __name__ == "__main__":
     prueba()
